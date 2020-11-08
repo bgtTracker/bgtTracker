456b29498fcc55c403a108d658f095918959caca
lorlen <filip.lew975@gmail.com>
2020-11-09 00:25:11 +0100

    initial commit

---
+++ src/main/java/pl/edu/pw/bgtTracker/BgtTrackerApplication.java
@@ -0,0 +1,13 @@
+package pl.edu.pw.bgtTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BgtTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BgtTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/edu/pw/bgtTracker/BgtTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.edu.pw.bgtTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BgtTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
